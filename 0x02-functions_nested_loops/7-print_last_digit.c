0e5d4014e2905776d437a4dc08af02da15775b27
tigawanna <denniskinuiaw@gmail.com>
2022-03-15 09:14:04 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
-
-
 /**
- * print_last_digit - prunt lats number
- *@c: The int to convert to absolute
- * Return: last
+ * print_last_digit - prints the last digit of a number
+ * @n: arg
+ * Return: value of the last digit using moduls
  */
-int print_last_digit(int c)
+int print_last_digit(int n)
 {
-if (c < 0)
-{
-int absVal = c + (c * -2);
-return (absVal % 10);
-}
+int a;
 
-else
-return (c % 10);
+if (n < 0)
+n = -n;
 
+a = n % 10;
 
+if (a < 0)
+a = -a;
+
+_putchar(a + '0');
+
+return (a);
 }
 
