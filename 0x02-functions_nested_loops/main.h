d975f0329b08786a68d22b2e8123575b3c3a469c
tigawanna <denniskinuiaw@gmail.com>
2022-03-15 02:39:37 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+void print_alphabet(void);
+int _putchar(char c);
+
+#endif
+
---
+++ 0x02-functions_nested_loops/method.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet -prints out alphabet
+ *
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+for (char x = 'a'; x < 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
+
