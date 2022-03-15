618587d803cebab54566a719e5f4b15fd67cc4f8
tigawanna <denniskinuiaw@gmail.com>
2022-03-15 03:06:06 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MY_HEADER
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _putchar(char c);
 
 #endif
-
--- 0x02-functions_nested_loops/method.c
+++ 0x02-functions_nested_loops/method.c
@@ -1,16 +1,34 @@
 #include "main.h"
+
 /**
  * print_alphabet -prints out alphabet
  *
  * Return: void
-*/
+ */
 
 void print_alphabet(void)
+
 {
 for (char x = 'a'; x < 'z'; x++)
 {
 _putchar(x);
 }
 _putchar('\n');
+}
+/**
+ * print_alphabet_x10 -prints out alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+for (int x = 0; x < 10; x++)
+{
+for (char x = 'a'; x < 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
 }
 
