f3e2a090c3f0d90f1fe5fff8bbb06ab8ccdd5b50
tigawanna <denniskinuiaw@gmail.com>
2022-03-15 04:15:00 -0700

    3-islower.c

--- 0x02-functions_nested_loops/method.c
+++ 0x02-functions_nested_loops/method.c
@@ -32,3 +32,26 @@ _putchar('\n');
 }
 }
 
+
+/**
+ * _islower -check if number in upper of lowwer case
+ *@c: The character to print
+ * Return: 1 for upper 0 for lower
+ */
+int _islower(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else if (c >= 72 && c <= 122)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+
+}
+
