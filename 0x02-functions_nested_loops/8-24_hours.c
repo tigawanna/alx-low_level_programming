595dd61911823f9fc9fa6943741cf413ededae85
tigawanna <denniskinuiaw@gmail.com>
2022-03-15 09:34:41 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,28 +4,26 @@
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
  */
-
 void jack_bauer(void)
 {
-int h,m;
-for(h = 0;h < 24;h++)
-{
-for(m = 0;m < 60;m++)
-{
+int i, j;
 
-_putchar((h / 10) + '0');
-_putchar((h % 10) + '0');
-_putchar(':');
-_putchar((m / 10) + '0');
-_putchar((m % 10) + '0');
-_putchar('\n');
+i = 0;
 
-}
-}
-_putchar(2 + '0');
-_putchar(4 + '0');
+while (i < 24)
+{
+j = 0;
+while (j < 60)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
 _putchar(':');
-_putchar(0 + '0');
-_putchar(0 + '0');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 _putchar('\n');
+j++;
 }
+i++;
+}
+}
+
