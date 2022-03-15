13a223e1072075e881383ba93d9da042443c7be6
tigawanna <denniskinuiaw@gmail.com>
2022-03-15 05:28:41 -0700

    5-sign.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,8 @@ void print_alphabet_x10(void);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
+
 
 #endif
+
--- 0x02-functions_nested_loops/method.c
+++ 0x02-functions_nested_loops/method.c
@@ -75,3 +75,30 @@ return (0);
 
 
 }
+
+
+/**
+ * print_sign -check if number positive negative or neutral
+ *@n: The character to print
+ * Return: 1 for true 0 for false
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar(0);
+return (0);
+}
+}
+
