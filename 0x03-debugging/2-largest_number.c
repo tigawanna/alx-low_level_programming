3c6fa58fe2b8f270ba0ee4b432c393901378a18b
tigawanna <denniskinuiaw@gmail.com>
2022-03-15 23:46:51 -0700

    task-2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,24 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)
- */
-void positive_or_negative(int n)
-{
-
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-
-}
-
 #include "main.h"
 
 /**
