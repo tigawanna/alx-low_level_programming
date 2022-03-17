a95e00e0680728b565d19f91219dcf6e238727a8
tigawanna <denniskinuthiaw@gmail.com>
2022-03-17 08:28:22 +0300

    task-1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if uppercase.
+ * @c: ascii value to check
+ *
+ * Return: 1 if true else 0
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+return (1);
+
+else
+return (0);
+}
