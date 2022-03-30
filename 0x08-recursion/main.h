17557f48e9f4714cbffe4e6d675193b765c599ae
tigawanna <denniskinuthiaw@gmail.com>
2022-03-30 11:23:47 +0300

    task-2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 
 #endif /* MAIN_H */
