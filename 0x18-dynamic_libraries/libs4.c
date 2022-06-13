6534927a4359ed9f3730227e9d56aaf6d388d973
tigawanna <denniskinuthiaw@gmail.com>
2022-06-13 09:05:43 +0300

    tasks 1

---
+++ 0x18-dynamic_libraries/libs4.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n) { return ("four"); }
+char *_strchr(char *s, char c) { return ("four"); }
+unsigned int _strspn(char *s, char *accept) { return (4); }
+char *_strpbrk(char *s, char *accept) { return ("four"); }
+char *_strstr(char *haystack, char *needle) { return ("four"); }
